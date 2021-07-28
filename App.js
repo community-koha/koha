9b9c986fbb27ae9b90c98dc27529abdad905fdf0
Joshua Soong <joshua@soong.nz>
2021-07-28 15:08:26 +1200

    Got barebones MapView working

--- App.js
+++ App.js
@@ -1,15 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Koha</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import React, { useState, useEffect } from 'react';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import MapView from 'react-native-maps';
+import * as Location from 'expo-location';
 
 const styles = StyleSheet.create({
   container: {
@@ -18,4 +13,128 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
 });
+
+function NotifScreen(e) {
+  return (
+    <View style={styles.container}>
+      <Text>{e}</Text>
+    </View>
+  );
+}
+
+function Map() {
+  const [locationResult, setLocation] = useState(null)
+  const [mapRegion, setRegion] = useState(null)
+  const [hasLocationPermissions, setLocationPermission] = useState(false)
+
+  useEffect(() => {
+    const getLocationAsync = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if ('granted' !== status) {
+        setLocation('Permission to access location was denied')
+      } else {
+        setLocationPermission(true);
+      }
+
+      let { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync({})
+      setLocation(JSON.stringify({ latitude, longitude }))
+
+      // Center the map on the location we just fetched.
+      setRegion({ latitude, longitude, latitudeDelta: 0.0922, longitudeDelta: 0.0421 });
+    }
+
+    getLocationAsync()
+  })
+
+  if (locationResult === null) {
+    NotifScreen("Loading")
+  }
+
+  if (hasLocationPermissions === false) {
+    NotifScreen("Location Permissions not granted")
+  }
+
+  if (mapRegion === null) {
+    NotifScreen("Invalid Map Region")
+  }
+  return (
+    <MapView
+      style={styles.container}
+      region={mapRegion}
+    >
+    </MapView>
+  );
+}
+
+function Listings() {
+  return (
+    <View style={styles.container}>
+      <Text>Listings WIP</Text>
+    </View>
+  );
+}
+
+function Create() {
+  return (
+    <View style={styles.container}>
+      <Text>Create WIP</Text>
+    </View>
+  );
+}
+
+const Tab = createMaterialBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Map"
+      activeColor="#FF8782"
+      labelStyle={{ fontSize: 12 }}
+      style={{ backgroundColor: '#fff' }}
+    >
+      <Tab.Screen
+        name="Map"
+        component={Map}
+        options={{
+          tabBarLabel: 'Area Map',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="map" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Listings"
+        component={Listings}
+        options={{
+          tabBarLabel: 'Listings',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="bulletin-board" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Create"
+        component={Create}
+        options={{
+          tabBarLabel: 'New Listing',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="tag-plus-outline" color={color} size={26} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyTabs/>
+    </NavigationContainer>
+  );
+}
