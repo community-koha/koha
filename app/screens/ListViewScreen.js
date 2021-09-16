03af649d245103a3fe046ad954026827f6fa6efc
Shanine <nuevashanine@gmail.com>
2021-09-17 07:58:39 +1200

    Setup states for components

--- app/screens/ListViewScreen.js
+++ app/screens/ListViewScreen.js
@@ -1,23 +1,31 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-
+import React, { Component, useState, useEffect } from 'react';
+import { StyleSheet, View, Text, ActivityIndicator, ScrollView } from 'react-native';
+import { ListItem } from 'react-native-elements';
+import { useNavigation } from '@react-navigation/native';
 import Colours from '../config/colours.js';
+import firebase from 'firebase/app';
 
-function ListViewScreen() {
+
+function ListViewScreen(){
+    
+    const [loading, setLoading] = useState(true); // Set loading to true on component mount
+    const [listings, setListings] = useState([]); // Initial empty array of users
+  
+    
+    }
     return (
-        <View style={styles.container}>
-            <Text>Listings WIP</Text>
-        </View>
-    );
+            <View style={styles.container}>
+            
+            </View>
+        )
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: Colours.white,
-        alignItems: 'center',
-        justifyContent: 'center',
-    }
+        paddingBottom: 22
+       }
+    
 })
 
 export default ListViewScreen;
