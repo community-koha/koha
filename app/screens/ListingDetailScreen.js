55e6c51db19ad201b657882b326dec2ebc2f3080
Ethan-Johnson-NZ <68883555+Ethan-Johnson-NZ@users.noreply.github.com>
2021-09-18 18:15:06 +1200

    Moved loading and result return together, fixed reloading issues.

--- app/screens/ListingDetailScreen.js
+++ app/screens/ListingDetailScreen.js
@@ -11,50 +11,56 @@ function ListingDetailScreen({route, navigation}){
     const [listings, setListings] = useState([]);
   
     useEffect(() => {
-      const subscriber = firebase.firestore()
-        .collection('listings')
-        .where(firebase.firestore.FieldPath.documentId(), '==', listingId)
-        .onSnapshot(querySnapshot => {
-            const listings = [];
-      
-            querySnapshot.forEach(documentSnapshot => {
-              listings.push({
-                ...documentSnapshot.data(),
-                key: documentSnapshot.id,
-              });
+        setLoading(true);
+        const subscriber = firebase.firestore()
+            .collection('listings')
+            .where(firebase.firestore.FieldPath.documentId(), '==', listingId)
+            .onSnapshot(querySnapshot => {
+                const listings = [];
+        
+                querySnapshot.forEach(documentSnapshot => {
+                listings.push({
+                    ...documentSnapshot.data(),
+                    key: documentSnapshot.id,
+                });
+                });
+        
+                setListings(listings);
+                setLoading(false);
             });
-      
-            setListings(listings);
-            setLoading(false);
-          });
   
       // Unsubscribe from events when no longer in use
       return () => subscriber();
-    }, []);
+    }, [listingId]);
   
-    if (loading) {
-      return <ActivityIndicator />;
-    }
-
     return (
         <ScrollView>
             <View style={styles.container}>
                 {
-                listings.map((item, i) => {
-                    return (
-                        <ListItem key={i} >
-                            <ListItem.Content>
-                                <ListItem.Title>{item.listingTitle}</ListItem.Title>
-                                <ListItem.Subtitle>{item.description}</ListItem.Subtitle>
-                                <ListItem.Subtitle>Location: {item.location["name"]} ({item.location["lat"]},{item.location["lng"]})</ListItem.Subtitle>
-                                <ListItem.Subtitle>Quantity: {item.quantity}</ListItem.Subtitle>
-                                <ListItem.Subtitle>Collection Method: {item.collectionMethod}</ListItem.Subtitle>
-                                <ListItem.Subtitle>Category: {item.category}</ListItem.Subtitle>
-                                <ListItem.Subtitle>Sub Category{item.subCategory}</ListItem.Subtitle>
-                            </ListItem.Content>
-                        </ListItem>
-                    );
-                    })
+                    loading &&
+                    (
+                        <ActivityIndicator />
+                    )
+                }
+                {
+                    !loading &&
+                    (
+                        listings.map((item, i) => {
+                            return (
+                                <ListItem key={i} >
+                                    <ListItem.Content>
+                                        <ListItem.Title>{item.listingTitle}</ListItem.Title>
+                                        <ListItem.Subtitle>{item.description}</ListItem.Subtitle>
+                                        <ListItem.Subtitle>Location: {item.location["name"]} ({item.location["lat"]},{item.location["lng"]})</ListItem.Subtitle>
+                                        <ListItem.Subtitle>Quantity: {item.quantity}</ListItem.Subtitle>
+                                        <ListItem.Subtitle>Collection Method: {item.collectionMethod}</ListItem.Subtitle>
+                                        <ListItem.Subtitle>Category: {item.category}</ListItem.Subtitle>
+                                        <ListItem.Subtitle>Sub Category{item.subCategory}</ListItem.Subtitle>
+                                    </ListItem.Content>
+                                </ListItem>
+                            );
+                        })
+                    )
                 }
             
             </View>
@@ -62,7 +68,7 @@ function ListingDetailScreen({route, navigation}){
                 <Button style={styles.button} title="Go Back" onPress={() => navigation.navigate('ListViewScreen')}></Button>
             </View>
         </ScrollView>
-        )
+    )
 }
 
 const styles = StyleSheet.create({
