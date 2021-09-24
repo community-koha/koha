1cd010c6e079a7eb45d98ef8c928b2bd1dd048f5
Ethan-Johnson-NZ <68883555+Ethan-Johnson-NZ@users.noreply.github.com>
2021-09-25 01:23:40 +1200

    Add simple button to sign out user in the profile screen
    Does not redirect back to the login screen since I couldn't get it working

--- app/screens/Profile.js
+++ app/screens/Profile.js
@@ -6,12 +6,21 @@ import {
 	View,
 	StyleSheet,
 	Text,
+	TouchableOpacity,
 } from 'react-native';
+
+import firebase from 'firebase/app';
+import 'firebase';
 
 function Profile({ navigation }) {
 	return (
 		<View style={styles.container}>
 			<Text>Profile Page</Text>
+			<TouchableOpacity
+				style={styles.button}
+				onPress={() => {firebase.auth().signOut(); navigation.goBack();}}>
+				<Text style={styles.buttonText}>Log Out</Text>
+			</TouchableOpacity>
 		</View>
 	);
 }
@@ -23,7 +32,20 @@ const styles = StyleSheet.create({
 		alignItems: 'center',
 		justifyContent: 'center'
 	},
-	
+	button: {
+		marginTop: Gui.screen.height * 0.1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		width: Gui.screen.width * 0.2,
+		height: Gui.screen.height * 0.1,
+		borderRadius: 3,
+		borderWidth: 1,
+		borderColor: Colours.default,
+	},
+	buttonText: {
+		fontSize: Gui.screen.height * 0.015,
+		fontWeight: 'bold',
+	},	
 });
 
 export default Profile;
