04263a2b59ffd1600a15c1edc11223d440cd65e9
Shanine <nuevashanine@gmail.com>
2021-09-27 01:12:43 +1300

    added buttons for different forms

--- app/screens/GiveKoha.js
+++ app/screens/GiveKoha.js
@@ -1,19 +1,31 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 import Colours from '../config/colours.js';
 import gui from '../config/gui.js';
 
-function GiveKoha(){
+function GiveKoha({navigation}){
     return(
-        <View>
-
+        <View style={styles.container}>
+            <View style={styles.buttonContainer}>
+                <Button title="Food" onPress={()=>navigation.navigate("NewFoodListing")}/>
+                <Button title="Essentials"/>
+                {/* Below buttons to be hidden for individual */}
+                <Button title="Event"/>
+                <Button title="Services"/>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-
+    container: {
+        flex: 1,
+        backgroundColor: Colours.white,
+    },
+    buttonContainer:{
+        paddingTop: gui.screen.height * 0.1,
+    }
 })
 
 export default GiveKoha;
