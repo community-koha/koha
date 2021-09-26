a72007b7f1f388f0d7a8c692d2f51fe855280faa
Shanine <nuevashanine@gmail.com>
2021-09-27 00:37:20 +1300

    created new screen for selecting koha type

---
+++ app/screens/GiveKoha.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Colours from '../config/colours.js';
+import gui from '../config/gui.js';
+
+function GiveKoha(){
+    return(
+        <View>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default GiveKoha;
